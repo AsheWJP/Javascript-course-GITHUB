bc7ee0f2dd0160df11680329b2ab1c712de19d56
AsheWJP <willashe98@gmail.com>
2025-06-12 09:35:43 -0400

    Added practice exercise

--- ControlFlow/control_flow.js
+++ ControlFlow/control_flow.js
@@ -45,3 +45,17 @@ console.log("User Category:", userCategory);
 let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 console.log("Authentication status:", authenticationStatus);
+
+let dietaryRole = "Employee";
+let dietaryAccess;
+
+if(dietaryRole === "Employee"){
+    dietaryAccess = "Welcome Employee! Access to Dietary Services fully granted.";
+} else if(dietaryRole === "Enrolled Member"){
+    dietaryAccess = "Welcome Enrolled Member! Full access to Dietary services and One-on-One interactions with dieticians granted.";
+}else if(dietaryRole === "Subscriber"){
+    dietaryAccess = "Welcome Subscriber! Partial access to Dietary services granted, please enroll for full access!";
+} else{
+    dietaryAccess = "You are not subscribed! To access Dietary services please subscribe or feel free to enroll as a member for full access to dieticians! Thank you!";
+}
+console.log("Access to dietary services:", dietaryAccess);
